4711a090496c12097da5eebe87961ef3d558be84
Tashifur Rahman <tashifur23@gmail.com>
2026-07-25 01:56:35 +0530

    added env

--- config.py
+++ config.py
@@ -1,5 +1,19 @@
-MODEL = "qwen3:8b"
+import os
+from dotenv import load_dotenv
+load_dotenv()
+MODEL = os.getenv(
+    "MODEL",
+    "qwen3:8b"
+)
 
-FILE_NAME = "data/chat_history.json"
+FILE_NAME = os.getenv(
+    "FILE_NAME",
+    "data/chat_history.json"
+)
 
-MAX_MESSAGES = 20
+MAX_MESSAGES = int(
+    os.getenv(
+        "MAX_MESSAGES",
+        20
+    )
+)
